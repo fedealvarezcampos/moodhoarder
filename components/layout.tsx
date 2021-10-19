86b308c1582b0a167195d45eb9c5569d7418db7e
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-10-19 17:04:56 +0200

    Some styling, nav link, animations

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,8 @@
 import { PropsWithChildren, ReactNode } from 'react';
-import styles from '../styles/layout.module.css';
 import Image from 'next/image';
-import Link from 'next/image';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import styles from '../styles/layout.module.css';
 
 type ComponentWithChildProps = PropsWithChildren<{ children: ReactNode }>;
 
@@ -9,18 +10,25 @@ export default function Layout({ children }: ComponentWithChildProps) {
     return (
         <>
             <div className={styles.container}>
-                {/* <Link href="/"> */}
                 <nav className={styles.logo}>
-                    <Image
-                        src="/images/logo.svg"
-                        alt="moodhoarder logo"
-                        width="100%"
-                        height="100%"
-                        layout="intrinsic"
-                        // objectFit="fill"
-                    />
+                    <Link href="/" passHref>
+                        <motion.div
+                            initial={{ x: -30, opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            transition={{ duration: 0.5 }}
+                            whileHover={{ scale: 0.98, transition: { duration: 0.2 } }}
+                            whileTap={{ scale: 1.0, transition: { duration: 0.2 } }}
+                        >
+                            <Image
+                                src="/images/logo.svg"
+                                alt="moodhoarder logo"
+                                width="100%"
+                                height="100%"
+                                layout="intrinsic"
+                            />
+                        </motion.div>
+                    </Link>
                 </nav>
-                {/* </Link> */}
                 <main className={styles.main}>{children}</main>
             </div>
         </>
