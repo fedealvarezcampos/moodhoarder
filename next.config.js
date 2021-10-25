26105b7ee9321061b5754ccd653b80bc9f67d985
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-10-25 13:53:38 +0200

    Dependencies config, analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
 module.exports = {
     reactStrictMode: true,
     images: {
         domains: ['bluhemglezuxswtcifom.supabase.in'],
     },
 };
+
+module.exports = withBundleAnalyzer({
+    reactStrictMode: true,
+});
