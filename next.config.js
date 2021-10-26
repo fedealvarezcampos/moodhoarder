a55ad52d15cd27dd6382187f8bad3837cab5cb09
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-10-26 02:26:56 +0200

    Config changes, content security, robots.txt

--- next.config.js
+++ next.config.js
@@ -1,5 +1,43 @@
 /** @type {import('next').NextConfig} */
 
+const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline' *.youtube.com *.supabase.in *.vercel.app *.vercel.com;
+    child-src *.youtube.com *.google.com;
+    style-src 'self' 'unsafe-inline' *.googleapis.com;
+    img-src * blob: data:;
+    media-src 'self';
+    font-src 'self' https://fonts.gstatic.com;
+    connect-src *;
+`;
+
+const securityHeaders = [
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\n/g, ''),
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'strict-origin-when-cross-origin',
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+    },
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()',
+    },
+];
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
@@ -8,5 +46,13 @@ module.exports = withBundleAnalyzer({
     reactStrictMode: true,
     images: {
         domains: ['bluhemglezuxswtcifom.supabase.in'],
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
     },
 });
