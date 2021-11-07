2e353ec60fb0801580e12343c93d3266da1ca8e3
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-11-07 15:12:25 +0100

    Favicon changes, svg added

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,8 @@ const Home: NextPage = ({ images, setImages }: any) => {
             <Head>
                 <title>moodhoarder</title>
                 <meta name="description" content="Moodhoarder | Create your own moodboards" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
             </Head>
 
             <Uploader images={images} setImages={setImages} />
