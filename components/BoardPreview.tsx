3e6490cf51f94adecc01fe82df3cdd4014728340
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-01-06 01:44:49 +0100

    MyBoards loading state, fixing some types

--- components/BoardPreview.tsx
+++ components/BoardPreview.tsx
@@ -2,44 +2,40 @@ import styles from '../styles/BoardPreview.module.css';
 import { supabaseHost } from '../lib/constants';
 import { boardItemAnimation } from '../styles/framer';
 import { motion } from 'framer-motion';
+import { Board } from '../pages/myboards';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export interface boardPreview {
-    boardItem: {
-        id: number;
-        uuid: string;
-        images: string[];
-        board_title: string;
-    };
+	boardItem: Board;
 }
 [];
 
 function BoardPreview({ boardItem }: boardPreview) {
-    return (
-        <Link href={'/' + boardItem?.uuid} passHref>
-            <motion.li
-                whileHover={{ scale: 1.05, cursor: 'pointer', backgroundColor: 'var(--pink)' }}
-                whileTap={{ scale: 1.02 }}
-                variants={boardItemAnimation}
-                className={styles.boardItemContainer}
-                key={boardItem?.id}
-            >
-                <div className={styles.imageContainer}>
-                    <Image
-                        src={supabaseHost + boardItem?.images[0]}
-                        layout="fill"
-                        objectFit="cover"
-                        alt="image in board"
-                        quality={35}
-                        placeholder="blur"
-                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOMNgYAAO8AkE7ayCwAAAAASUVORK5CYII="
-                    />
-                </div>
-                <div className={styles.boardTitle}>{boardItem?.board_title || 'untitled'} </div>
-            </motion.li>
-        </Link>
-    );
+	return (
+		<Link href={'/' + boardItem?.uuid} passHref>
+			<motion.li
+				whileHover={{ scale: 1.05, cursor: 'pointer', backgroundColor: 'var(--pink)' }}
+				whileTap={{ scale: 1.02 }}
+				variants={boardItemAnimation}
+				className={styles.boardItemContainer}
+				key={boardItem?.id}
+			>
+				<div className={styles.imageContainer}>
+					<Image
+						src={supabaseHost + boardItem?.images[0]}
+						layout="fill"
+						objectFit="cover"
+						alt="image in board"
+						quality={35}
+						placeholder="blur"
+						blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOMNgYAAO8AkE7ayCwAAAAASUVORK5CYII="
+					/>
+				</div>
+				<div className={styles.boardTitle}>{boardItem?.board_title || 'untitled'} </div>
+			</motion.li>
+		</Link>
+	);
 }
 
 export default BoardPreview;
--- pages/myboards.tsx
+++ pages/myboards.tsx
@@ -8,98 +8,111 @@ import { supabase } from '../lib/supabaseClient';
 import { IoCaretDownOutline } from '@react-icons/all-files/io5/IoCaretDownOutline';
 import { IoCaretUpOutline } from '@react-icons/all-files/io5/IoCaretUpOutline';
 import { notifyError } from '../assets/toasts';
+import Spinner from '../assets/spinner';
 import BoardPreview from '../components/BoardPreview';
 import styles from '../styles/myboards.module.css';
 
+export interface Board {
+	id: number;
+	board_title: string;
+	images: string[];
+	uuid: string;
+	owner_uid: string;
+}
+
 const MyBoards: NextPage = () => {
-    const router = useRouter();
-    const user = supabase.auth.user();
+	const router = useRouter();
+	const user = supabase.auth.user();
 
-    const [myBoards, setMyBoards] = useState<any>();
-    const [order, setOrder] = useState<boolean>(false);
+	const [myBoards, setMyBoards] = useState<Board[]>();
+	const [order, setOrder] = useState<boolean>(false);
+	const [loading, setLoading] = useState(false);
 
-    const getBoards = async (order: boolean) => {
-        try {
-            const { data, error }: any = await supabase
-                .from('boards')
-                .select('id, board_title, images, uuid, owner_uid')
-                .in('owner_uid', [user?.id])
-                .order('created_at', { ascending: order });
+	const getBoards = async (order: boolean) => {
+		try {
+			setLoading(true);
 
-            if (error) {
-                throw error;
-            }
+			const { data, error }: any = await supabase
+				.from('boards')
+				.select('id, board_title, images, uuid, owner_uid')
+				.in('owner_uid', [user?.id])
+				.order('created_at', { ascending: order });
 
-            setMyBoards(data);
-        } catch (error: any) {
-            notifyError(error.message);
-        }
-    };
+			if (error) throw error;
 
-    useEffect(() => {
-        if (!user) router.push('/');
-        user && getBoards(order);
-    }, [user, order]);
+			setMyBoards(data);
+		} catch (error: any) {
+			notifyError(error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-    return (
-        <>
-            <Head>
-                <title>moodhoarder | my boards</title>
-            </Head>
+	useEffect(() => {
+		if (!user) router.push('/');
+		user && getBoards(order);
+	}, [user, order]);
 
-            {myBoards && myBoards?.length !== 0 && (
-                <>
-                    <motion.div
-                        initial={{ y: -30, opacity: 0 }}
-                        animate={{ y: 0, opacity: 1 }}
-                        className={styles.pageTitle}
-                    >
-                        These are all your saved boards
-                    </motion.div>
-                    <motion.button
-                        whileHover={{ y: -2 }}
-                        whileTap={{ y: -1 }}
-                        transition={{ duration: 0.2 }}
-                        className={styles.sortButton}
-                        onClick={() => setOrder(!order)}
-                    >
-                        {order ? (
-                            <span>
-                                <IoCaretDownOutline />
-                                Oldest to newest
-                            </span>
-                        ) : (
-                            <span>
-                                <IoCaretUpOutline />
-                                Newest to oldest
-                            </span>
-                        )}
-                    </motion.button>
-                </>
-            )}
-            {myBoards && myBoards?.length === 0 && (
-                <motion.div
-                    initial={{ y: -30, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    className={styles.pageTitle}
-                >
-                    Your boards would be here... if you had any.
-                </motion.div>
-            )}
-            {myBoards && (
-                <motion.ul
-                    initial="initial"
-                    animate="animate"
-                    variants={stagger}
-                    className={styles.boardsContainer}
-                >
-                    {myBoards?.map((item: any) => (
-                        <BoardPreview boardItem={item} key={item?.id} />
-                    ))}
-                </motion.ul>
-            )}
-        </>
-    );
+	return (
+		<>
+			<Head>
+				<title>moodhoarder | my boards</title>
+			</Head>
+
+			{loading && <Spinner />}
+			{myBoards && myBoards?.length !== 0 && !loading && (
+				<>
+					<motion.div
+						initial={{ y: -30, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						className={styles.pageTitle}
+					>
+						These are all your saved boards
+					</motion.div>
+					<motion.button
+						whileHover={{ y: -2 }}
+						whileTap={{ y: -1 }}
+						transition={{ duration: 0.2 }}
+						className={styles.sortButton}
+						onClick={() => setOrder(!order)}
+					>
+						{order ? (
+							<span>
+								<IoCaretDownOutline />
+								Oldest to newest
+							</span>
+						) : (
+							<span>
+								<IoCaretUpOutline />
+								Newest to oldest
+							</span>
+						)}
+					</motion.button>
+				</>
+			)}
+			{myBoards && myBoards?.length === 0 && !loading && (
+				<motion.div
+					initial={{ y: -30, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					className={styles.pageTitle}
+				>
+					Your boards would be here... if you had any.
+				</motion.div>
+			)}
+			{myBoards && !loading && (
+				<motion.ul
+					initial="initial"
+					animate="animate"
+					variants={stagger}
+					className={styles.boardsContainer}
+				>
+					{myBoards?.map((item: Board) => (
+						<BoardPreview boardItem={item} key={item?.id} />
+					))}
+				</motion.ul>
+			)}
+		</>
+	);
 };
 
 export default MyBoards;
